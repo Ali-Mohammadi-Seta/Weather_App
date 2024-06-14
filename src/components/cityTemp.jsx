099c1fa7359d85fde6259d699ffabe0ad5b4119a
Ali <alimohammadiseta@gmail.com>
2024-06-14 20:34:42 +0330

    Added Error and Loading Icon

--- src/components/cityTemp.jsx
+++ src/components/cityTemp.jsx
@@ -1,6 +1,6 @@
 // cityTemp.jsx
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 import cloudsDay from '../assets/CloudsDay.jpg'
 import cloudsNight from '../assets/CloudsNight.jpg'
 import rainNight from '../assets/RainNight.jpg'
@@ -13,35 +13,35 @@ import hazeDay from '../assets/HazeDay.jpg'
 import hazeNight from '../assets/HazeNight.jpg'
 
 const CityTemp = () => {
-    const { loading, data, error } = useSelector((state) => state);
-
-
-
+    const {loading, data, error} = useSelector((state) => state);
 
 
     const getLocalTime = (timezoneOffset) => {
         const now = new Date();
         const utcTime = now.getTime() + now.getTimezoneOffset() * 60000; // Convert to UTC
         const localTime = new Date(utcTime + timezoneOffset * 1000); // Adjust to local time
-        return localTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Format time
+        return localTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}); // Format time
     };
-
 
 
     const dayTime = {
-        Rain: { backgroundImage: `url(${rainDay})` },
-        Clear: { backgroundImage: `url(${clearDay})` },
-        Clouds: { backgroundImage: `url(${cloudsDay})` },
-        Snow: { backgroundImage: `url(${snowDay})` },
-        Haze: { backgroundImage: `url(${hazeDay})` },
+        Rain: {backgroundImage: `url(${rainDay})`},
+        Clear: {backgroundImage: `url(${clearDay})`},
+        Clouds: {backgroundImage: `url(${cloudsDay})`},
+        Snow: {backgroundImage: `url(${snowDay})`},
+        Haze: {backgroundImage: `url(${hazeDay})`},
+        // Fog:{},
+        // sand:{},
     };
 
     const nightTime = {
-        Rain: { backgroundImage: `url(${rainNight})` },
-        Clear: { backgroundImage: `url(${clearNight})` },
-        Clouds: { backgroundImage: `url(${cloudsNight})` },
-        Snow: { backgroundImage: `url(${snowNight})` },
-        Haze: { backgroundImage: `url(${hazeNight})` },
+        Rain: {backgroundImage: `url(${rainNight})`},
+        Clear: {backgroundImage: `url(${clearNight})`},
+        Clouds: {backgroundImage: `url(${cloudsNight})`},
+        Snow: {backgroundImage: `url(${snowNight})`},
+        Haze: {backgroundImage: `url(${hazeNight})`},
+        // Fog:{},
+        // sand:{},
     };
     const getBackgroundStyle = () => {
         const weatherMain = data.weather && data.weather.length > 0 ? data.weather[0].main : null;
@@ -58,15 +58,14 @@ const CityTemp = () => {
     };
 
 
-
-
-
     const animation = {
         Rain: "https://lottie.host/5eafa51a-a952-449b-a4e2-625095b0373f/5Gtwfit4gG.json",
         Clear: "https://lottie.host/ff14d40f-7962-4ff6-8179-90f0653f61f4/orsgLjt11R.json",
         Clouds: "https://lottie.host/34b26663-65b0-424c-9a86-6a27d2da7ddd/wJuHuuy5lU.json",
         Snow: "https://lottie.host/00b4eb1f-d297-4e89-a01a-18e1a68c59f8/kr8lKhDuvx.json",
-        Haze : "https://lottie.host/6f7d9f26-ef38-4761-bcdc-e9dc055f40e4/Pkcenpqsb1.json",
+        Haze: "https://lottie.host/6f7d9f26-ef38-4761-bcdc-e9dc055f40e4/Pkcenpqsb1.json",
+        // Fog:{},
+        // sand:{},
     };
 
     useEffect(() => {
@@ -82,39 +81,57 @@ const CityTemp = () => {
     const animationUrl = getAnimationUrl();
 
 
-
     return (
-        <div className='flex justify-center mx-auto max-w-[1640px] border rounded h-screen' style={{ ...getBackgroundStyle(), backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} >
+
+
+
+        <div className='items-center mx-auto max-w-[1640px]  text-black'
+             style={{...getBackgroundStyle(), backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
             {loading ? (
-                <div>Loading...</div>
+                <div>Loading...
+                    <span className="loading loading-dots loading-md"></span></div>
             ) : error ? (
-                <div>Error: {error}</div>
+                <div>
+                    <div role="alert" className="alert alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none"
+                             viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+                        <span>{error}</span>
+                    </div></div>
             ) : (
-                <div className='border mx-auto text-center'>
-                    {animationUrl && (
-                        <dotlottie-player
-                            src={animationUrl}
-                            background="transparent"
-                            speed="1"
-                            style={{ width: 300, height: 300 }}
-                            loop
-                            autoplay
-                        ></dotlottie-player>
-                    )}
-                    <h1 className='font-bold text-center text-5xl'>{data.name}</h1>
+                <div className=' text-center'>
+                    <h1 className='font-bold text-center text-4xl lg:text-7xl md:text-5xl'>{data.name}</h1>
+                    <div className='flex justify-center items-center'>
+                        {animationUrl && (
+                            <dotlottie-player
+                                src={animationUrl}
+                                background="transparent"
+                                speed="1"
+                                style={{width: 300, height: 300}}
+                                loop
+                                autoplay
+                            ></dotlottie-player>
+                        )}</div>
+
                     {data.main && (
-                        <div className=''>
-                            <p className='pb-5 pt-5 text-center text-7xl'>
+                        <div className='items-center'>
+                            <p className='pb-10 pt-5 text-center text-4xl lg:text-7xl md:text-5xl font-bold'>
                                 {data.main.temp}°C
                             </p>
-                            <p className='pb-5'>Weather: {data.weather[0].main}</p>
-                            <p className='pb-5'>Humidity: {data.main.humidity}%</p>
-                            <p className='pb-5'>Wind Speed: {data.wind.speed} MPH</p>
-                            <p className='pb-5'>
-                                Feels Like: {data.main.feels_like}°C
-                            </p>
-                            <p>Local Time: {getLocalTime(data.timezone)}</p>
+
+                            <div className='grid grid-cols-1 gap-6  mx-auto border  justify-items-center '>
+                                <p className='pb-5 border rounded w-[300px] glass '>Weather: {data.weather[0].main}</p>
+                                <p className='pb-5 border rounded w-[300px] glass'>Humidity: {data.main.humidity}%</p>
+                                <p className='pb-5 border rounded w-[300px] glass '>Wind Speed: {data.wind.speed} MPH</p>
+                                <p className='pb-5 border rounded w-[300px] glass'>
+                                    Feels Like: {data.main.feels_like}°C
+                                </p>
+                                <p className="pb-5 border rounded w-[300px] glass">Local Time: {getLocalTime(data.timezone)}</p>
+                            </div>
                         </div>
+
                     )}
                 </div>
             )}
